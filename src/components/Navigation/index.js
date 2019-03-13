b0dc36549be031678e6979a41ef381ba7dcb88e5
Paul Standley <paulstandley1972@gmail.com>
2019-03-13 15:58:33 +0000

    setup files

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navigation = () => (
+  <div>
+    <h2>Navigation</h2>
+  </div>
+);
+
+export default Navigation;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUp = () => (
+  <div>
+    <h2>SignUp</h2>
+  </div>
+);
+
+export default SignUp;
