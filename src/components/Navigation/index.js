9d6b3026f523d7f069efa5ac36e28bf9a5cac0a2
Paul Standley <paulstandley1972@gmail.com>
2019-03-13 18:42:44 +0000

    add styled components and routes

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+
+import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
-  <div>
-    <h2>Navigation</h2>
-  </div>
+  <Nav>
+    <ul>
+      <Li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </Li>
+      <Li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </Li>
+      <Li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </Li>
+      <Li>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+      </Li>
+      <Li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </Li>
+    </ul>
+  </Nav>
 );
+
+const Nav = styled.nav`
+  padding: 1rem 0.8rem;
+  text-align: center;
+  color: var(--mainfontscolor);
+  background-color: var(--mainheadercolor);
+  ul {
+    list-style-type: none;
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`
+const Li = styled.li`
+  background: transparent;
+  border-radius: 9px;
+  border: 2px solid var(--mainfontscolor);
+  color: var(--mainfontscolor);
+  padding: 0.5rem 4rem;
+  cursor: pointer;
+  transition: all 1s ease-in-out;
+  &:hover {
+    background-color: var(--mainLink);
+    color: var(--mainvapbgcolor);
+    border: 2px solid var(--bodybgcolor);
+  }
+  &:focus {
+    outline: none;
+  }
+  ${props => 
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`
 
 export default Navigation;
