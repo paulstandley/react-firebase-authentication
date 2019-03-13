c2d032a5de6ef687da628b4e054998a288d7d82e
Paul Standley <paulstandley1972@gmail.com>
2019-03-13 23:25:23 +0000

    Fix

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import  { FirebaseContext } from '../Firebase';
 import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
+import * as ROUTES from '../../constants/routes';;
 
 const SignUpPage = () => (
   <div>
@@ -20,7 +22,7 @@ const INITIAL_STATE = {
   error: null,
 };
 
-class SignUpForm extends Component {
+class SignUpFormBase extends Component {
   
   constructor(props) {
     super(props);
@@ -103,6 +105,11 @@ const SignUpLink = () => (
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
+
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 
